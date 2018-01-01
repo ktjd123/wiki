6c8a343645590b56b530dec413e6160a67a1de2c
ktjd1262 <k9326239@gmail.com>
2018-01-01 18:26:54 +0900

    minor fix

--- frontend/src/containers/About.js
+++ frontend/src/containers/About.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import queryString from 'query-string';
 
 const About = ({location, match}) => {
-    const query = queryString.parse(location.search)
     return(
         <div>
             <h2>About {match.params.name}</h2>
