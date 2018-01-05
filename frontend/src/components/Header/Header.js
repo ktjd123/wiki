fa3841ebe060e41df55e8471a23236a9eec6f0d2
ktjd1262 <k9326239@gmail.com>
2018-01-06 00:42:30 +0900

    header done with reactive

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,10 +1,38 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
+import './Header.css'
 
 class Header extends Component {
     render() {
         return (
-            <div>
-                
+            <div className='header'>
+                <div className='left'>
+                    <Link to='/' className='logo'>WIKI</Link>
+                    <Link to='/' className='recent'>
+                        <i className='material-icons'>access_time</i>
+                        <div>최근변경</div>
+                    </Link>
+                    <Link to='/' className='argument'>
+                        <i className='material-icons'>chat</i>
+                        <div>최근토론</div>
+                    </Link>
+                    <Link to='/' className='special'>
+                        <i className='material-icons'>settings</i>
+                        <div>특수기능</div>
+                    </Link>
+                    <Link to='/' className='random'>
+                        <i className='material-icons'>shuffle</i>
+                        <div>랜덤문서</div>
+                    </Link>
+                </div>
+                <div className='center'></div>
+                <div className='right'>
+                    <div className='search'>
+                        <input />
+                        <a className='material-icons'>search</a>
+                    </div>
+                    <a className='material-icons'>person</a>
+                </div>
             </div>
         );
     }
--- frontend/src/components/Template/MainTemplate/MainTemplate.js
+++ frontend/src/components/Template/MainTemplate/MainTemplate.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Header } from 'components'
+
 import './MainTemplate.css'
 
-const MainTemplate = ({App}) => {
+const MainTemplate = ({ App }) => {
     return (
         <div className='mainTemplate'>
+            <Header />
             <section className='app'>
                 {App}
             </section>
