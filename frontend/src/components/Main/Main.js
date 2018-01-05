66d8d614261424d1a071870a9184a09b8e64ce35
ktjd1262 <k9326239@gmail.com>
2018-01-06 01:25:03 +0900

    parsertest

--- frontend/src/components/Main/Main.js
+++ frontend/src/components/Main/Main.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import './Main.css';
 
 
 const Main = ({ testString, reducerTest }) => {
+
+    let string = "[[[ 그래서 그게 어쨌는데? ]]]  쏼라쏼라 [[[또왜]]] <br>"
     return (
         <div>
             <div className="App">
                 <h1 className="App-title">The Seed에 어서오세요</h1>
                 <p className="App-intro">
-                    모든 것을 이곳에서부터 시작하세요!<br/>
+                    모든 것을 이곳에서부터 시작하세요!<br />
                     Start everything from here
                 </p>
                 {testString}
@@ -21,22 +23,15 @@ const Main = ({ testString, reducerTest }) => {
                 </button>
                 </div>
                 <div className="title">사용된 기술들</div>
-                <ul className="introduce">
-                    <li>---BACKEND---</li>
-                    <li>express</li>
-                    <li>express session</li>
-                    <li>mongoose</li>
-                    <li>bcrypt</li>
-                    <li>예정: multer</li>
-                    <li>---FRONTEND---</li>
-                    <li>React</li>
-                    <li>Redux</li>
-                    <li>Create-react-app</li>
-                    <li>react-router</li>
-                    <li>redux-thunk</li>
-                    <li>axios</li>
-                    <li>immutable</li>
-                </ul>
+                {
+                    string.split('[[[').map((inner, i) => {
+                        inner = inner.replace(']]]', '')
+                        if (i === 0) {
+                            return (<div className='cancel'>{inner}</div>)
+                        }
+                        return 0;
+                    })
+                }
             </div>
             <Link to='/about'>About</Link>
         </div>
