53c4411aa5aa018e9305f9c68cb03b0691a7cd26
ktjd1262 <k9326239@gmail.com>
2018-01-10 18:05:07 +0900

    parser

---
+++ frontend/src/components/Parser/Parser.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Parser.css'
+
+class Parser extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default Parser;
